c5cb4148f8dc20bc408e50a95737ca25d22b2942
kevkatam <kevkatam@gmail.com>
2024-04-15 22:18:31 +0300

    modified basic_auth.py

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -7,4 +7,18 @@ from .auth import Auth
 
 class BasicAuth(Auth):
     """ basic auth class """
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str
+            ) -> str:
+        """ returns Bae64 part of the authorization header for basic auth
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) != str:
+            return None
+
+        a_header = authorization_header.split(' ')
+        if a_header[0] != "Basic":
+            return None
+        return a_header[1]
