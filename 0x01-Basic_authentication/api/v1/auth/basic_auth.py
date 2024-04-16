923d1fe8a63eb0d2e808d8907b776d7f47e955fe
kevkatam <kevkatam@gmail.com>
2024-04-16 16:24:36 +0300

    modified basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -53,7 +53,8 @@ class BasicAuth(Auth):
         if ":" not in decoded_base64_authorization_header:
             return (None, None)
 
-        credentials = decoded_base64_authorization_header.split(':')
+        credentials = decoded_base64_authorization_header.split(':', 1)
+        email, pwd = credentials
         return (credentials[0], credentials[1])
 
     def user_object_from_credentials(
