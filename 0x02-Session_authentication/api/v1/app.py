29794c726c46328a3f10ce71947ab034bc955acd
kevkatam <kevkatam@gmail.com>
2024-04-20 12:17:41 +0300

    modified app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,12 +32,16 @@ def before_request() -> None:
 
     if auth is None:
         return
-    mylist = [
-        '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'
+    ex_list = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
         ]
-    if auth.require_auth(request.path, mylist) is False:
+    if auth.require_auth(request.path, ex_list) is False:
         return
-    if auth.authorization_header(request) is None:
+    if (auth.authorization_header(request) and auth.session_cookie(request)):
+        return None
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
