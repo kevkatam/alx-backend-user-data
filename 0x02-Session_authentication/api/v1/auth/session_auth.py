4818ee898da4917f264f1d012e6ace754b2aca4f
kevkatam <kevkatam@gmail.com>
2024-04-20 09:58:35 +0300

    modified app.py, adding session_auth.py

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Session authentication module
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """ class for creating a new authentication mechanism """
+    pass
