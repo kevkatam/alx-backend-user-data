c81df7bea9c27f7940fc4a01517dd77e455ce02c
kevkatam <kevkatam@gmail.com>
2024-04-24 17:24:16 +0300

    assing user.py

---
+++ 0x03-user_authentication_servic/README.md
@@ -0,0 +1,10 @@
+# User authentication service
+
+Project done during alx software engineering programme short specialization backend.
+
+# Aim
+
+- How to declare API routes in a Flask app
+- How to get and set cookies
+- How to retrieve request form data
+- How to return various HTTP status codes
---
+++ 0x03-user_authentication_servic/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+user module
+"""
+from sqlalchemy import Integer, String, Column, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ user class """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
