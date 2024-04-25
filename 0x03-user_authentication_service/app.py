ec91248621a612df669b8ae5ea9b4c2b07ee46e8
kevkatam <kevkatam@gmail.com>
2024-04-25 17:46:10 +0300

    modified app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -52,7 +52,7 @@ def login() -> str:
 
 
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
-def logout() ->:
+def logout() -> None:
     """ deletes the current user's session """
     session_id = request.form.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
