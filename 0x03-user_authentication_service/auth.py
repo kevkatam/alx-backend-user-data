20eb0e76138206176c3020d7ca74a5a8f9eea36c
kevkatam <kevkatam@gmail.com>
2024-04-25 10:57:26 +0300

    modified auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
  _hashed_password function module
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -10,3 +13,22 @@ def _hash_password(password: str) -> bytes:
     b_password = password.encode()
     hashed = bcrypt.hashpw(b_password, bcrypt.gensalt())
     return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ hashes user password and save user to database """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError(f'User {email} already exists')
+
+        hashed = _hashed_password(password)
+        new_user = self._db.add_user(email, hashed)
+        return new_user
