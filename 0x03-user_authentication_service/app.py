85d52e3c19fa05254d18cff601d47c656157782e
kevkatam <kevkatam@gmail.com>
2024-04-25 11:28:51 +0300

    adding app.py

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+simple flask app module
+"""
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_data():
+    """ returns a JSON payload """
+    data = {"message": "Bienvenue"}
+
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
