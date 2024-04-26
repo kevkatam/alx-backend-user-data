7424772b8c512dc1dbad0f1d6a44e20c08dd1744
kevkatam <kevkatam@gmail.com>
2024-04-26 11:15:25 +0300

    modified auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,6 +87,7 @@ class Auth:
             raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
+        """ updates the user password with particular reset """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_password = _hash_password(password)
